3aa026230858aaf7713d5b0dedb7796b57971608
xjantoth <toth.janci@gmail.com>
2024-10-16 15:11:37 +0200

    Product Factory

--- project-factory/main.tf
+++ project-factory/main.tf
@@ -41,28 +41,60 @@ module "projects" {
   data_overrides = {
     prefix = var.prefix
   }
-  factories_config = merge(local.factories_config, {
+    factories_config = {
+    # budgets = {
+    #   billing_account   = var.billing_account_id
+    #   budgets_data_path = "data/budgets"
+    #   notification_channels = {
+    #     billing-default = {
+    #       project_id = "foo-billing-audit"
+    #       type       = "email"
+    #       labels = {
+    #         email_address = "gcp-billing-admins@example.org"
+    #       }
+    #     }
+    #   }
+    # }
+    folders_data_path  = "data/hierarchy"
+    projects_data_path = "data/projects"
     context = {
-      folder_ids = merge(
-        { for k, v in var.folder_ids : k => v if v != null },
-        var.factories_config.context.folder_ids
-      )
-      iam_principals = merge(
-        {
-          for k, v in var.service_accounts :
-          k => "serviceAccount:${v}" if v != null
-        },
-        var.groups,
-        var.factories_config.context.iam_principals
-      )
-      tag_values = merge(
-        var.tag_values,
-        var.factories_config.context.tag_values
-      )
-      vpc_host_projects = merge(
-        var.host_project_ids,
-        var.factories_config.context.vpc_host_projects
-      )
+      folder_ids = {
+        default = "folders/238132938234"
+        teams   = "folders/238132938234"
+      }
+      # iam_principals = {
+      #   gcp-devops = "group:gcp-devops@example.org"
+      # }
+      # tag_values = {
+      #   "org-policies/drs-allow-all" = "tagValues/123456"
+      # }
+      # vpc_host_projects = {
+      #   dev-spoke-0 = "test-pf-dev-net-spoke-0"
+      # }
     }
-  })
+  }
+  # factories_config = merge(local.factories_config, {
+  #   context = {
+  #     folder_ids = merge(
+  #       { for k, v in var.folder_ids : k => v if v != null },
+  #       var.factories_config.context.folder_ids
+  #     )
+  #     iam_principals = merge(
+  #       {
+  #         for k, v in var.service_accounts :
+  #         k => "serviceAccount:${v}" if v != null
+  #       },
+  #       var.groups,
+  #       var.factories_config.context.iam_principals
+  #     )
+  #     tag_values = merge(
+  #       var.tag_values,
+  #       var.factories_config.context.tag_values
+  #     )
+  #     vpc_host_projects = merge(
+  #       var.host_project_ids,
+  #       var.factories_config.context.vpc_host_projects
+  #     )
+  #   }
+  # })
 }
