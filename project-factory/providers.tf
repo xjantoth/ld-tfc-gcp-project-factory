74c5cd4375b8b3f80272757026918ab5f73cc8ee
xjantoth <toth.janci@gmail.com>
2024-10-24 12:44:14 +0200

    clean up

--- project-factory/providers.tf
+++ project-factory/providers.tf
@@ -5,8 +5,3 @@ provider "google" {
 provider "google-beta" {
   impersonate_service_account = "demo-executor-0@mystic-airway-438411-a1.iam.gserviceaccount.com"
 }
-
-# variable "project_id" {
-#   type        = string
-#   description = "Project ID."
-# }
